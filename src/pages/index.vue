6029c7918e55bde8ad73a5da2045c576568330d5
Sanshiroh SAKAI <sansyo@gmail.com>
2020-08-30 19:35:26 +0900

    基本ページをすべて初期化

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,63 +1,35 @@
 <template lang="pug">
-.container
+div
   h1
-    | contentful のデータを取得して表示確認
-  div.entries(v-for="post in posts" :key="post.sys.id")
-    p
-      | ID：{{ post.sys.id }}
-    p
-      | 更新日：{{ post.sys.updatedAt }}
-    p
-      | タイトル：{{ post.fields.title }}
-    p
-      | スラッグ：
-      nuxt-link(:to="`/spot/${post.fields.slug}`")
-        | {{ post.fields.slug }}
-    div(v-html="$md.render(post.fields.description)")
-    p
-      | エリア：{{ post.fields.area.fields.name }}（{{ post.fields.area.fields.slug }}）
-    div
-      | カテゴリ：
-      ul
-        li(
-          v-for="(category, index) in post.fields.categories"
-          :key="category.fields.slug"
-        )
-          | {{ category.fields.name }}（{{ category.fields.slug }}）
-    p
-      | Google Map：
-      a(:href="post.fields.googleMap", target='_blank', rel='noopener noreferrer')
-        | {{ post.fields.googleMap }}
+    | トップです
+
+//- .container
+//-   h1
+//-     | contentful のデータを取得して表示確認
+//-   div.entries(v-for="post in posts" :key="post.sys.id")
+//-     p
+//-       | ID：{{ post.sys.id }}
+//-     p
+//-       | 更新日：{{ post.sys.updatedAt }}
+//-     p
+//-       | タイトル：{{ post.fields.title }}
+//-     p
+//-       | スラッグ：
+//-       nuxt-link(:to="`/spot/${post.fields.slug}`")
+//-         | {{ post.fields.slug }}
+//-     div(v-html="$md.render(post.fields.description)")
+//-     p
+//-       | エリア：{{ post.fields.area.fields.name }}（{{ post.fields.area.fields.slug }}）
+//-     div
+//-       | カテゴリ：
+//-       ul
+//-         li(
+//-           v-for="(category, index) in post.fields.categories"
+//-           :key="category.fields.slug"
+//-         )
+//-           | {{ category.fields.name }}（{{ category.fields.slug }}）
+//-     p
+//-       | Google Map：
+//-       a(:href="post.fields.googleMap", target='_blank', rel='noopener noreferrer')
+//-         | {{ post.fields.googleMap }}
 </template>
-
-<script>
-import contentfulClient from '@/plugins/contentful'
-
-export default {
-  asyncData({ env }) {
-    return contentfulClient
-      .getEntries({
-        content_type: 'spot' // コンテンツモデル `spot` の全エントリを取得
-      })
-      .then((entries) => {
-        return {
-          posts: entries.items
-        }
-      })
-  }
-}
-</script>
-
-<style lang="scss">
-body {
-  padding: 3em;
-}
-
-.entries {
-  margin: 2em 0 0;
-
-  hr {
-    margin: 2em 0;
-  }
-}
-</style>
--- src/pages/spot/_slug.vue
+++ src/pages/spot/_slug.vue
@@ -1,36 +1,7 @@
 <template lang="pug">
-div.container
-  h1
-    | {{ post.fields.title }}
-
+div
+  //- h1
+  //-   | {{ post.fields.title }}
   p
     nuxt-link(to="/") トップへ
 </template>
-
-<script>
-import contentfulClient from '@/plugins/contentful'
-
-export default {
-  asyncData({ env, params }) {
-    return contentfulClient
-      .getEntries({
-        content_type: 'spot' // コンテンツモデル `spot` の全エントリを取得
-      })
-      .then((entries) => {
-        return {
-          posts: entries.items,
-          slug: params.slug
-        }
-      })
-  },
-  computed: {
-    post() {
-      return this.posts.find(post => post.fields.slug === this.slug)
-    }
-  },
-}
-</script>
-
-<style lang="scss" scoped>
-
-</style>
