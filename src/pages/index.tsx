0ae54928d6472878f12053db1b761987dfce2f11
Sanshiroh SAKAI <sansyo@gmail.com>
2023-01-29 12:12:49 +0900

    get api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,58 @@
-export default function Home() {
+import { client } from '@/lib/api'
+
+import type { Category, Blog, Area } from '@/types/api/blog'
+
+// microCMS へ API リクエスト
+export const getStaticProps = async () => {
+  try {
+    const blog = await client.get({
+      endpoint: 'blogs',
+    })
+    const categories = await client.get({
+      endpoint: 'categories',
+      queries: {
+        fields: 'id,name,slug',
+        limit: 30,
+      },
+    })
+    const areas = await client.get({
+      endpoint: 'areas',
+      queries: {
+        fields: 'id,name,slug',
+        limit: 30,
+      },
+    })
+
+    return {
+      props: {
+        blogs: blog.contents,
+        categories: categories.contents,
+        areas: areas.contents,
+      },
+    }
+  } catch (err) {
+    console.log('-- getPosts --')
+    console.log(err)
+  }
+}
+
+// Props の型
+type Props = {
+  blogs: Blog[]
+  categories: Category[]
+  areas: Area[]
+}
+
+const Home = ({ blogs, categories, areas }: Props) => {
+  console.log(blogs)
+  console.log(categories)
+  console.log(areas)
+
   return (
-    <>
-      <main>
-        <h1>京都グルメ</h1>
-      </main>
-    </>
+    <main>
+      <h1>京都グルメ</h1>
+    </main>
   )
 }
+
+export default Home
