cb5566bb5a97319c01dd211560a8d437450ed3aa
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-02-01 22:36:44 +0900

    記事一覧ページ作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,39 @@
-export default function Home() {
+import Link from 'next/link'
+
+import { map } from 'lodash-es'
+
+import { client } from '@/libs/client'
+import type { Blog } from '@/types/microcms'
+
+type Props = {
+  blogs: Blog[]
+}
+
+export const getServerSideProps = async () => {
+  const data = await client.get({
+    endpoint: 'blogs',
+  })
+
+  return {
+    props: {
+      blogs: data.contents,
+    },
+  }
+}
+
+const Home = ({ blogs }: Props) => {
   return (
     <>
       <h1>記事一覧</h1>
       <ul>
-        <li>
-          <a href="#">記事タイトル</a>
-        </li>
-        <li>
-          <a href="#">記事タイトル</a>
-        </li>
-        <li>
-          <a href="#">記事タイトル</a>
-        </li>
+        {map(blogs, (post) => (
+          <li key={post.id}>
+            <Link href={`/spot/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   )
 }
+
+export default Home
