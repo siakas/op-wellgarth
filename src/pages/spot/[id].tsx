603ca9548117a00362a36dfd670d695b303b074f
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-02-02 14:36:50 +0900

    getStaticPaths と getStaticProps を使って詳細ページを作り直し

--- src/pages/spot/[id].tsx
+++ src/pages/spot/[id].tsx
@@ -1,6 +1,8 @@
+import type { GetStaticPaths, GetStaticProps } from 'next'
 import Link from 'next/link'
 
 import parse from 'html-react-parser'
+import { map } from 'lodash-es'
 
 import { client } from '@/libs/client'
 import type { Blog } from '@/types/microcms'
@@ -9,8 +11,24 @@ type Props = {
   blog: Blog
 }
 
-export const getServerSideProps = async (ctx: any) => {
-  const id = ctx.params?.id
+// 静的生成のためのパスを指定
+export const getStaticPaths: GetStaticPaths = async () => {
+  const data = await client.get({
+    endpoint: 'blogs',
+    queries: { limit: 50 },
+  })
+
+  const paths = map(data.contents, (i) => `/spot/${i.id}`)
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+// データをテンプレートに受け渡す部分の処理を記述
+export const getStaticProps: GetStaticProps = async (context) => {
+  const id = context.params?.id
   const idExceptArray = id instanceof Array ? id[0] : id
   const data = await client.get({
     endpoint: 'blogs',
@@ -24,25 +42,34 @@ export const getServerSideProps = async (ctx: any) => {
   }
 }
 
-const Spot = ({ blog }: Props) => {
+const BlogId = ({ blog }: Props) => {
   return (
-    <>
-      <div>
-        <h1>{blog.title}</h1>
+    <main>
+      <h1>{blog.title}</h1>
 
-        {blog.eyecatch && (
-          <div>
-            <img src={blog.eyecatch.url} alt="" width={800} />
-          </div>
-        )}
+      {blog.eyecatch && (
+        <div>
+          <img src={blog.eyecatch.url} alt="" width={600} />
+        </div>
+      )}
 
-        {blog.content !== undefined && <div>{parse(blog.content)}</div>}
-      </div>
+      {blog.content !== undefined && <div>{parse(blog.content)}</div>}
+
+      <h2>カテゴリ</h2>
+      <ul>
+        {map(blog.categories, (category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+
+      <h2>エリア</h2>
+      <p>{blog.area.name}</p>
+
       <div>
         <Link href="/">一覧へ戻る</Link>
       </div>
-    </>
+    </main>
   )
 }
 
-export default Spot
+export default BlogId
