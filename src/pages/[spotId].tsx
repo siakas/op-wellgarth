c2b2da9f655d63fb3d3bedd72891ef51d83afe00
Sanshiroh SAKAI <sansyo@gmail.com>
2023-03-21 17:41:29 +0900

    ISR の不具合修正

--- src/pages/[spotId].tsx
+++ src/pages/[spotId].tsx
@@ -41,7 +41,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
--- src/pages/category/[categoryId]/page/[id].tsx
+++ src/pages/category/[categoryId]/page/[id].tsx
@@ -68,7 +68,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: 'blocking',
   }
 }
 
