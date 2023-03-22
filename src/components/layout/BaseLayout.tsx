13d043985030d1f8563e335d6ef0a82dcc36c90c
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-03-22 18:24:34 +0900

    同一ページコンポーネントによるダイナミックルーティング間の遷移でもトランジションが実行されるよう修正

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,6 +1,7 @@
 import type { FC, ReactNode } from 'react'
 import { Container, Flex } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
+import { useRouter } from 'next/router'
 import Footer from '@/components/layout/Footer'
 import Header from '@/components/layout/Header'
 
@@ -16,9 +17,12 @@ const fadeInOut = {
 }
 
 const BaseLayout: FC<BaseLayoutProps> = ({ children }) => {
+  const router = useRouter()
+
   return (
     <>
       <motion.div
+        key={router.asPath}
         initial="initial"
         animate="animate"
         exit="exit"
