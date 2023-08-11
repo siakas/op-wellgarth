e0a767c31a69dc19b9bad18295d20c0e08c1346e
Sanshiroh SAKAI <sansyo@gmail.com>
2023-08-11 15:43:41 +0900

    旧データ削除

--- src/libs/client.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from 'microcms-js-sdk'
-import { siteConfig } from '@@/site.config'
-
-export const client = createClient({
-  serviceDomain: siteConfig.serviceId,
-  apiKey: siteConfig.apiKey,
-})
